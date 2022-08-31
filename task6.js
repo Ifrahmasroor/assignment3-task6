d15b5ac16c77bb5fc5c3fbc838b1dfc3fdc9f902
ifrah <110095465+Ifrahmasroor@users.noreply.github.com>
2022-08-31 11:21:12 +0500

    Add files via upload

---
+++ task6.js
@@ -0,0 +1,16 @@
+let random  = Math.floor (Math.random()*5);
+console.log(random);
+ const a = prompt("enter a number");
+console.log(a);
+ if(a<5)
+ {
+    console.log("your number is less then 5");
+}else if(a>5)
+{
+  console.log("your number is greater  then 5");
+}
+else if(a === "5" )
+{
+  console.log("your number is equal t0 5");
+
+}
